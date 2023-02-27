9dbb094fa24730fd67aaba6fe5466a0979fbc8f8
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-02-27 23:59:23 +0900

    hook : register and watch

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 /* function ToDoList() {
   const [toDo, setToDo] = useState("");
@@ -27,5 +28,23 @@ import React, { useState } from "react";
     </div>
   );
 } */
+
+function ToDoList() {
+  const { register, watch } = useForm();
+  console.log(watch());
+  return (
+    <div>
+      <form>
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("firstName")} placeholder="First Name" />
+        <input {...register("lastName")} placeholder="Last Name" />
+        <input {...register("username")} placeholder="Username" />
+        <input {...register("password")} placeholder="Password" />
+        <input {...register("password1")} placeholder="Password1" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
 
 export default ToDoList;
